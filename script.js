ea4b12055c441422ee77f26873c19b2046aa05e1
John Dawes <jdawes@bluebitmedia.com>
2018-08-05 14:15:48 -0600

    Jest expect array test

--- script.js
+++ script.js
@@ -8,8 +8,8 @@ const googleDatabase = [
   'myfavouritecatsandmore.com'
 ];
 
-const googleSearch = searchInput => {
-  const matches = googleDatabase.filter(website => {
+const googleSearch = (searchInput, db) => {
+  const matches = db.filter(website => {
     return website.includes(searchInput);
   });
 
@@ -17,4 +17,6 @@ const googleSearch = searchInput => {
   return matches.length > 3 ? matches.slice(0, 3) : matches;
 };
 
-console.log(googleSearch('cat'));
+//console.log(googleSearch('cat', googleDatabase));
+
+module.exports = googleSearch;
--- script.test.js
+++ script.test.js
@@ -1,1 +1,12 @@
-it('This is a test', () => {});
+const googleSearch = require('./script');
+
+dbMock = ['dog.com', 'cheesepuff.com', 'disney.com', 'dogpictures.com'];
+
+it('Hello test', () => {
+  expect('hello').toBe('hello');
+});
+
+it('is searching google', () => {
+  expect(googleSearch('testtest', dbMock)).toEqual([]);
+  expect(googleSearch('dog', dbMock)).toEqual(['dog.com', 'dogpictures.com']);
+});
