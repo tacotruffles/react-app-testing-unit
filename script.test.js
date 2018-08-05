c0d30a67e11e8f91b8e46469e3292151f032a00f
John Dawes <jdawes@bluebitmedia.com>
2018-08-05 14:22:45 -0600

    Test grouping

--- script.test.js
+++ script.test.js
@@ -6,7 +6,18 @@ it('Hello test', () => {
   expect('hello').toBe('hello');
 });
 
-it('is searching google', () => {
-  expect(googleSearch('testtest', dbMock)).toEqual([]);
-  expect(googleSearch('dog', dbMock)).toEqual(['dog.com', 'dogpictures.com']);
+describe('googleSearch', () => {
+  it('is searching google', () => {
+    expect(googleSearch('testtest', dbMock)).toEqual([]);
+    expect(googleSearch('dog', dbMock)).toEqual(['dog.com', 'dogpictures.com']);
+  });
+
+  it('works with undefined and null input', () => {
+    expect(googleSearch(undefined, dbMock)).toEqual([]);
+    expect(googleSearch(null, dbMock)).toEqual([]);
+  });
+
+  it('does not return more than 3 matches', () => {
+    expect(googleSearch('.com', dbMock).length).toEqual(3);
+  });
 });
