c78fee2c28c1086b736f0ec4b2a7214c3aecc087
John Dawes <jdawes@bluebitmedia.com>
2018-08-05 14:04:00 -0600

    Basic Jest configuration

---
+++ script.js
@@ -0,0 +1,20 @@
+const googleDatabase = [
+  'cats.com',
+  'souprecipes.com',
+  'flowers.com',
+  'animals.com',
+  'catpictures.com',
+  'myfavouritecats.com',
+  'myfavouritecatsandmore.com'
+];
+
+const googleSearch = searchInput => {
+  const matches = googleDatabase.filter(website => {
+    return website.includes(searchInput);
+  });
+
+  // Only return a max of three items.
+  return matches.length > 3 ? matches.slice(0, 3) : matches;
+};
+
+console.log(googleSearch('cat'));
