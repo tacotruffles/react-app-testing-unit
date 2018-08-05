bb0adf8a1035cc0a23dae9c4ea3b8c629987ea15
John Dawes <jdawes@bluebitmedia.com>
2018-08-05 15:11:40 -0600

    Async testing in Jest with expect.assertions

--- script2.js
+++ script2.js
@@ -1,1 +1,31 @@
 const fetch = require('node-fetch');
+const uri = 'https://swapi.co/api/people'; //'https://jsonplaceholder.typicode.com/users';
+
+const getStarWarsPeoplePromise = fetch => {
+  return fetch(uri)
+    .then(response => response.json())
+    .then(data => {
+      //console.log(data);
+      return {
+        count: data.count,
+        results: data.results
+      };
+    });
+};
+
+const getStarWarsPeopleAsync = async fetch => {
+  const getRequest = await fetch(uri);
+  const data = await getRequest.json();
+
+  //console.log(data);
+
+  return {
+    count: data.count,
+    results: data.results
+  };
+};
+
+module.exports = {
+  getStarWarsPeoplePromise,
+  getStarWarsPeopleAsync
+};
--- script2.test.js
+++ script2.test.js
@@ -1,1 +1,24 @@
 // Async testing
+const fetch = require('node-fetch');
+const swapi = require('./script2');
+
+it('calls swapi to get people through ASYNC call', done => {
+  expect.assertions(1);
+  swapi.getStarWarsPeopleAsync(fetch).then(data => {
+    //console.log('[ASYNC]: ', data);
+
+    expect(data.count).toEqual(87);
+    done();
+  });
+});
+
+it('calls swapi to get people with a PROMISE', done => {
+  expect.assertions(2);
+  // Use return for promises....easier!
+  return swapi.getStarWarsPeoplePromise(fetch).then(data => {
+    //console.log('[PROMISE]: ', data);
+    expect(data.count).toEqual(87);
+    expect(data.results.length).toBeGreaterThan(5);
+    done();
+  });
+});
